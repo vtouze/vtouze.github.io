7884d1b4a237333cc89d1faf742202ba2140a35d
TeeZ <54781489+TeeZ21@users.noreply.github.com>
2024-01-21 02:07:50 +0100

    Scroll Top Button

---
+++ script.js
@@ -0,0 +1,16 @@
+const goTopBtn = document.querySelector('.go-top-btn');
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight()
+{
+    if(window.scrollY > 1000)
+    {
+        goTopBtn.style.display = 'flex'
+    }
+    else
+    {
+        goTopBtn.style.display = 'none'
+    }
+}
+
+goTopBtn.addEventListener('click', () => {window.scrollTo({top:0, behavior:'smooth'})})
