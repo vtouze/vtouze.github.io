829deceec4f0deec79687ce1ae84c4e38b4a1f44
TeeZ <54781489+TeeZ21@users.noreply.github.com>
2024-01-21 04:16:20 +0100

    Transition Animations #1

--- script.js
+++ script.js
@@ -28,6 +28,32 @@ function parallax(e)
         const x = (window.innerWidth - e.pageX*speed)
         const y = (window.innerHeight - e.pageY*speed)
 
-        layer.style.transform = 'translateX(${x}px) translateY(${y}px)'
+        layer.style.transform = 'translateX(${x}px) translateY(${y}px)';
     });
+}
+
+/* Animations */
+
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    const anchors = document.querySelectorAll('a')
+
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 500);
+
+    for (let i = 0; i < anchors.length; i++){
+        const anchor = anchors[i];
+
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.target.href;
+
+            transition_el.classList.add("is-active");
+
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500);
+        });
+    }
 }
