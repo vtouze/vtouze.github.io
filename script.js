970b98728e2b484fa1a6f15ccf32bacc489af4dc
TeeZ <54781489+TeeZ21@users.noreply.github.com>
2024-01-21 03:13:26 +0100

    Clean

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Up Arrow */
+
 const goTopBtn = document.querySelector('.go-top-btn');
 window.addEventListener('scroll', checkHeight)
 
@@ -14,3 +16,18 @@ function checkHeight()
 }
 
 goTopBtn.addEventListener('click', () => {window.scrollTo({top:0, behavior:'smooth'})})
+
+/* Parralax */
+
+document.addEventListener('mousemove', parallax);
+function parallax(e)
+{
+    this.querySelectorAll('.layer').forEach(Layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)
+        const y = (window.innerHeight - e.pageY*speed)
+
+        layer.style.transform = 'translateX(${x}px) translateY(${y}px)'
+    });
+}
