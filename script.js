4f78e5f39b3dadd2e581a6a93d24261b25ed63d9
TeeZ <touzevirgile@gmail.com>
2024-03-07 20:28:42 +0100

    Fix Sidebar/Slider

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ window.onload = () => {
 
 /* Images Slider */
 
-let slider = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
+let slider = document.querySelector('.slider .slider-list');
+let items = document.querySelectorAll('.slider .slider-list .slider-item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 let dots = document.querySelectorAll('.slider .dots li');
